2a8e0a36a5b00541f5712aaac7095c3356d53c59
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-06 21:01:43 -0300

    feat: model UserType

--- src/main/java/com/client/ws/apispring/model/UserType.java
+++ src/main/java/com/client/ws/apispring/model/UserType.java
@@ -11,7 +11,40 @@ public class UserType  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_type_id")
-    private long id;
+    private Long id;
     private String nome;
     private String description;
+
+    public UserType() {
+    }
+
+    public UserType(Long id, String nome, String description) {
+        this.id = id;
+        this.nome = nome;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
