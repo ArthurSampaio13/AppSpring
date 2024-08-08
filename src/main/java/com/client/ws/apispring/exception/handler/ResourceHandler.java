9577f7985d0270aaf7a68c8a1982655ee3b185c8
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-08 17:02:40 -0300

    refac: retornando json de erros

--- src/main/java/com/client/ws/apispring/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/apispring/exception/handler/ResourceHandler.java
@@ -1,23 +1,51 @@
 package com.client.ws.apispring.exception.handler;
 
+import com.client.ws.apispring.dto.error.ErrorResponseDTO;
 import com.client.ws.apispring.exception.BadRequestException;
 import com.client.ws.apispring.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ResourceHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException n) {
-        String erroMessage = n.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erroMessage);
+    public ResponseEntity<ErrorResponseDTO> notFoundException(NotFoundException n) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDTO.builder()
+                        .message(n.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                .build());
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequestException(BadRequestException b) {
-        String erroMessage = b.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroMessage);
+    public ResponseEntity<ErrorResponseDTO> badRequestException(BadRequestException b) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDTO.builder()
+                .message(b.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> methodArgumentNotValidException(MethodArgumentNotValidException m) {
+        Map<String, String> messages = new HashMap<>();
+        m.getBindingResult().getAllErrors().forEach(error -> {String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            messages.put(field, message);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDTO.builder()
+                .message(Arrays.toString(messages.entrySet().toArray()))
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+
     }
 }
